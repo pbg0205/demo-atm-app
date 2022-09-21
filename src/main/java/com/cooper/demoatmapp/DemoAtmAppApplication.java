a73402b2be74caeb338c9fa42408a4534f0ca098
cooper <pbg0205@naver.com>
2022-09-22 00:15:13 +0900

    feat: atm app first commit

---
+++ src/main/java/com/cooper/demoatmapp/DemoAtmAppApplication.java
@@ -0,0 +1,13 @@
+package com.cooper.demoatmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAtmAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAtmAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cooper/demoatmapp/DemoAtmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooper.demoatmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAtmAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
